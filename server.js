8d607fdb40f256346c0b34e3abbe8f0d49bafc88
santosh gupta <santoshgupta@santoshs-MacBook-Pro.local>
2017-12-26 22:51:18 +0530

    Initial commit and add all file

---
+++ server.js
@@ -0,0 +1,56 @@
+
+const express=require('express');
+const hbs=require('hbs');
+const fs = require('fs');
+
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view Engine','hbs');
+
+
+app.use((req,res,next)=>{
+	var now =new Date().toString();
+	var log = `${now} : ${req.method} ${req.url}\n`;
+
+	console.log(log)
+	fs.appendFile('server.log',log);
+	next();
+});
+
+// app.use((req,res,next)=>{
+// 	res.render('maintenance.hbs');
+// });
+
+app.use(express.static(__dirname + '/public'));
+
+hbs.registerHelper('getCurrentYear',()=>{
+	return new Date().getFullYear();
+});
+
+hbs.registerHelper('streamIt',(text)=>{
+	return text.toUpperCase();
+});
+
+app.get('/',(req,res)=>{
+
+	res.render('home.hbs',{
+		pageTitle:'Welcome Page',
+		welcomeMessage:'Welcome to my site'
+	});
+});
+
+app.get('/about',(req,res)=>{
+	// res.send("About page");
+	res.render('about.hbs', {
+		pageTitle: 'About Page'
+	});
+});
+
+app.get('/bad',(req,res)=>{
+	res.send({
+		errorMessage:"Unable to handle Error Message"
+	});
+});
+
+app.listen(3000);
